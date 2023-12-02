1ed135d7961990b37e6e9f83e45333fb4e2e7514
Matt Machuga <machuga@gmail.com>
2023-12-02 11:51:00 -0500

    2023 Day 2 part 2

--- years/2023/day-02/aoc.ts
+++ years/2023/day-02/aoc.ts
@@ -60,7 +60,30 @@ const part1 = () => {
 };
 
 const part2 = () => {
-  const result = pipe([])(Deno.args[0]);
+  const result = pipe([
+    getInput,
+    map(parseLine),
+    map(pipe([
+      // Damn closure scoping forcing me to do this at the moment.  The reduce
+      // initializer value will be captured as a literal and will cause weird
+      // behavior if this is not wrapped in another execution context
+      (game) =>
+        reduce((acc, set) => {
+          Object.keys(possibleSet).forEach((color) => {
+            if (set[color] > acc[color]) {
+              acc[color] = set[color];
+            }
+          });
+          return acc;
+        }, { blue: 0, green: 0, red: 0 })(game),
+      (game) =>
+        Object.keys(possibleSet).reduce(
+          (acc, color) => acc * (game[color] || 1),
+          1,
+        ),
+    ])),
+    sum,
+  ])(Deno.args[0]);
 
   console.log("Part 2:", result);
 };
